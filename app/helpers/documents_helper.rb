e9d2f1729af976fad04c569259ba712a25690555
awoo452 <aaronwood8888@gmail.com>
2026-02-05 21:07:40 -0800

    1.23.30

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,5 +1,11 @@
 module DocumentsHelper
   def document_image_url(document, image)
-    s3_url("documents/#{document.id}/#{image['file']}")
+    file = image["file"].to_s
+    key = if file == "logo-classic.png"
+      "branding/logo-classic.png"
+    else
+      "documents/#{document.id}/#{file}"
+    end
+    s3_url(key)
   end
 end
